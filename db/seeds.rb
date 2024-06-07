6f570d260df8ff314799d8546b39218788fb626d
tama3br0 <newairport180@gmail.com>
2024-06-08 02:07:21 +0900

    seeds.rbの中身を調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,64 +33,64 @@ questions_data.each do |question_data|
   end
 end
 
-# 学校コード、学年、クラス番号の組み合わせを生成
-school_codes = [1, 2]
-grades = [1, 2]
-class_nums = [1, 2, 3]
-student_count = 30
+# # 学校コード、学年、クラス番号の組み合わせを生成
+# school_codes = [1, 2]
+# grades = [1, 2]
+# class_nums = [1, 2, 3]
+# student_count = 30
 
-# 初期データのクリエーション
-school_codes.each do |school_code|
-  grades.each do |grade|
-    class_nums.each do |class_num|
-      # grade_classesテーブルのエントリを作成
-      grade_class = GradeClass.find_or_create_by!(grade: grade, class_num: class_num, school_code: school_code)
+# # 初期データのクリエーション
+# school_codes.each do |school_code|
+#   grades.each do |grade|
+#     class_nums.each do |class_num|
+#       # grade_classesテーブルのエントリを作成
+#       grade_class = GradeClass.find_or_create_by!(grade: grade, class_num: class_num, school_code: school_code)
 
-      # 学生の作成
-      student_count.times do |student_num|
-        user = User.create!(
-          email: Faker::Internet.unique.email,
-          uid: SecureRandom.uuid,
-          provider: 'google',
-          role: 0,  # student
-          grade_class: grade_class,
-          student_num: student_num + 1,
-          password: 'password',
-          password_confirmation: 'password',
-          additional_info_provided: true
-        )
+#       # 学生の作成
+#       student_count.times do |student_num|
+#         user = User.create!(
+#           email: Faker::Internet.unique.email,
+#           uid: SecureRandom.uuid,
+#           provider: 'google',
+#           role: 0,  # student
+#           grade_class: grade_class,
+#           student_num: student_num + 1,
+#           password: 'password',
+#           password_confirmation: 'password',
+#           additional_info_provided: true
+#         )
 
-        # 日記の作成
-        diary = Diary.create!(user_id: user.id, date: Date.today)
+#         # 日記の作成
+#         diary = Diary.create!(user_id: user.id, date: Date.today)
 
-        # 質問に対する回答の作成
-        questions_data.each do |question_data|
-          question = Question.find_by(text: question_data["text"])
-          emotion_data = question_data["emotions"].sample
-          emotion = ChooseEmotion.find_by(text: emotion_data["text"], level: emotion_data["level"])
+#         # 質問に対する回答の作成
+#         questions_data.each do |question_data|
+#           question = Question.find_by(text: question_data["text"])
+#           emotion_data = question_data["emotions"].sample
+#           emotion = ChooseEmotion.find_by(text: emotion_data["text"], level: emotion_data["level"])
 
-          Answer.create!(diary_id: diary.id, question_id: question.id, choose_emotion_id: emotion.id)
-        end
+#           Answer.create!(diary_id: diary.id, question_id: question.id, choose_emotion_id: emotion.id)
+#         end
 
-        # スタンプの作成
-        Stamp.create!(user_id: user.id, diary_id: diary.id)
-      end
+#         # スタンプの作成
+#         Stamp.create!(user_id: user.id, diary_id: diary.id)
+#       end
 
-      # 教師の作成
-      teacher_names = ["鈴木 太郎", "田中 花子", "佐藤 次郎"]
-      teacher_names.each do |name|
-        User.create!(
-          email: Faker::Internet.unique.email,
-          uid: SecureRandom.uuid,
-          provider: 'google',
-          role: 1,  # teacher
-          grade_class: grade_class,
-          name: name,
-          password: 'password',
-          password_confirmation: 'password',
-          additional_info_provided: true
-        )
-      end
-    end
-  end
-end
+#       # 教師の作成
+#       teacher_names = ["鈴木 太郎", "田中 花子", "佐藤 次郎"]
+#       teacher_names.each do |name|
+#         User.create!(
+#           email: Faker::Internet.unique.email,
+#           uid: SecureRandom.uuid,
+#           provider: 'google',
+#           role: 1,  # teacher
+#           grade_class: grade_class,
+#           name: name,
+#           password: 'password',
+#           password_confirmation: 'password',
+#           additional_info_provided: true
+#         )
+#       end
+#     end
+#   end
+# end
