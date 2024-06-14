ca3ece04afc1d5e70791d855648ce5cc5970f7a5
tama3br0 <newairport180@gmail.com>
2024-06-14 15:28:28 +0900

    スタンプページが表示されない

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -4,7 +4,11 @@ class StampsController < ApplicationController
     def show
       @user = User.find(params[:id])
       @diaries = @user.diaries.includes(:stamps)
-      @date = Date.today.change(month: permitted_params[:month].to_i, year: permitted_params[:year].to_i) if permitted_params[:month] && permitted_params[:year]
+      if permitted_params[:month] && permitted_params[:year]
+        @date = Date.new(permitted_params[:year].to_i, permitted_params[:month].to_i)
+      else
+        @date = Date.today
+      end
     end
 
     private
