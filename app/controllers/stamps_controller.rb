7de619fc4eb2b1d081924cc55e5486531be41521
tama3br0 <newairport180@gmail.com>
2024-06-14 21:14:09 +0900

    スタンプカードページの修正

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -9,11 +9,30 @@ class StampsController < ApplicationController
       else
         @date = Date.today
       end
+      @calendar = generate_calendar(@date)
     end
 
     private
 
     def permitted_params
       params.permit(:month, :year)
+    end
+
+    def generate_calendar(date)
+      start_date = date.beginning_of_month.beginning_of_week(:sunday)
+      end_date = date.end_of_month.end_of_week(:sunday)
+      (start_date..end_date).map do |date|
+        [date, stamp_for_date(@user, date)]
+      end
+    end
+
+    def stamp_for_date(user, date)
+      # 日付に対するスタンプを取得するロジックをここに追加
+      # 例: user.diaries.find_by(date: date).present?
+      user.diaries.find_by(date: date).present? ? 'stamp_image_url' : nil
+    end
+
+    def japanese_day_names
+      %w[にち げつ か すい もく きん ど]
     end
 end
