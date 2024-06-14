d16c9bc465ae6e4644dbc7f666172356dc609fff
tama3br0 <newairport180@gmail.com>
2024-06-14 14:30:30 +0900

    スタンプカードのCSS調整

--- app/helpers/stamps_helper.rb
+++ app/helpers/stamps_helper.rb
@@ -1,2 +1,18 @@
 module StampsHelper
+    def japanese_day_names
+      %w[げつ か すい もく きん ど にち]
+    end
+
+    def japanese_month_calendar(options = {}, &block)
+      options[:day_names] = japanese_day_names
+      month_calendar(options, &block)
+    end
+
+    def display_user_info(user)
+      if user.teacher?
+        user.name
+      else
+        "#{user.grade}年#{user.class_num}組 #{user.student_num}番"
+      end
+    end
 end
