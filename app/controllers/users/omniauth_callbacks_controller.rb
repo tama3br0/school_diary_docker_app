4d7913f2b1dabd8b3c216864b5a682bddae5eca7
tama3br0 <newairport180@gmail.com>
2024-06-15 19:39:24 +0900

    ログインのフラッシュ…

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,11 +5,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         if @user.additional_info_provided
           sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :login_success) if is_navigational_format?
+          flash[:notice] = 'ログイン しました！' # set_flash_messageの代わりに直接設定
         else
           sign_in @user
           redirect_to additional_info_path
-          set_flash_message(:notice, :additional_info_required) if is_navigational_format?
+          flash[:notice] = 'データを とうろく しよう！' # set_flash_messageの代わりに直接設定
         end
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
