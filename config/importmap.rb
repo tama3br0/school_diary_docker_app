76aa1421c52384fb8e9ba893274dd4719f52b8a1
tama3br0 <newairport180@gmail.com>
2024-06-15 16:50:20 +0900

    いらないものを削除

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,5 +6,4 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "controllers/flash_controller", to: "controllers/flash_controller.js" # <= flashのために追記
 
