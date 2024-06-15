3d3f1616b918433a156b3246c56b6687ee776262
tama3br0 <newairport180@gmail.com>
2024-06-15 21:08:43 +0900

    にっきをみるページの仕様変更

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -2,54 +2,78 @@ class DiariesController < ApplicationController
     before_action :authenticate_user!
 
     def new
-      @diary = Diary.new
-      @questions = Question.all
-      @selected_answers = {}
+        @diary = Diary.new
+        @questions = Question.all
+        @selected_answers = {}
     end
 
     def create
-      current_user.diaries.where(date: diary_params[:date]).destroy_all
-      @diary = current_user.diaries.build(diary_params)
-      @questions = Question.all
-      @selected_answers = params[:answers] || {}
+        current_user.diaries.where(date: diary_params[:date]).destroy_all
+        @diary = current_user.diaries.build(diary_params)
+        @questions = Question.all
+        @selected_answers = params[:answers] || {}
 
-      if @selected_answers.empty? || @questions.any? { |q| @selected_answers[q.id.to_s].blank? }
-        flash.now[:alert] = 'こたえていない しつもんがあるよ'
-        render :new, status: :unprocessable_entity
-      else
-        if @diary.save
-          @selected_answers.each do |question_id, choose_emotion_id|
-            @diary.answers.create(question_id: question_id, choose_emotion_id: choose_emotion_id)
-          end
-          Stamp.create(user: current_user, diary: @diary)
-          redirect_to stamp_path(current_user.id), notice: 'にっきを ていしゅつしました！'
+        if @selected_answers.empty? || @questions.any? { |q| @selected_answers[q.id.to_s].blank? }
+            flash.now[:alert] = 'こたえていない しつもんがあるよ'
+            render :new, status: :unprocessable_entity
         else
-          flash.now[:alert] = @diary.errors.full_messages.join(', ')
-          render :new, status: :unprocessable_entity
+            if @diary.save
+                @selected_answers.each do |question_id, choose_emotion_id|
+                    @diary.answers.create(question_id: question_id, choose_emotion_id: choose_emotion_id)
+                end
+                Stamp.create(user: current_user, diary: @diary)
+                redirect_to stamp_path(current_user.id), notice: 'にっきを ていしゅつしました！'
+            else
+                flash.now[:alert] = @diary.errors.full_messages.join(', ')
+                render :new, status: :unprocessable_entity
+            end
         end
-      end
     end
 
     def choose_diary
-      @date = params[:date]&.to_date || Date.today
-      start_of_week = @date.beginning_of_week(:monday)
-      end_of_week = @date.end_of_week(:sunday)
+        @date = params[:date]&.to_date || Date.today
+        start_of_week = @date.beginning_of_week(:monday)
+        end_of_week = @date.end_of_week(:sunday)
 
-      @previous_week_range = (start_of_week - 7.days)..(end_of_week - 7.days + 1.day)
-      @current_week_range = start_of_week..(end_of_week + 1.day)
-      @next_week_range = (start_of_week + 7.days)..(end_of_week + 7.days + 1.day)
+        @previous_week_range = (start_of_week - 7.days)..(end_of_week - 7.days + 1.day)
+        @current_week_range = start_of_week..(end_of_week + 1.day)
+        @next_week_range = (start_of_week + 7.days)..(end_of_week + 7.days + 1.day)
 
-      @diaries = current_user.diaries.where(date: @current_week_range).order(:date)
+        @diaries = current_user.diaries.where(date: @current_week_range).order(:date)
+    end
+
+    def class_diary
+        @grade_class = GradeClass.find(params[:id])
+        @date = params[:date]&.to_date || Date.today
+        @previous_date = @date - 1.day
+        @next_date = @date + 1.day
+
+        @students = User.where(grade_class: @grade_class).order(:student_num)
+        @questions = Question.all
+    end
+
+    def student_diary
+        @student = User.find(params[:id])
+        @date = params[:date]&.to_date || Date.today
+        @previous_month = @date - 1.month
+        @next_month = @date + 1.month
+
+        @grade = @student.grade
+        @class_num = @student.class_num
+        @student_num = @student.student_number
+
+        @diaries = @student.diaries.where("date >= ? AND date <= ?", @date.beginning_of_month, @date.end_of_month).order(:date)
+        @questions = Question.all
     end
 
     def show
-      @diary = current_user.diaries.find(params[:id])
-      @questions = @diary.answers.includes(:question, :choose_emotion).map(&:question).uniq
+        @diary = current_user.diaries.find(params[:id])
+        @questions = @diary.answers.includes(:question, :choose_emotion).map(&:question).uniq
     end
 
     private
 
     def diary_params
-      params.require(:diary).permit(:date, :image_url, answers: {})
+        params.require(:diary).permit(:date, :image_url, answers: {})
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
     authenticated :user do
-        root to: 'home#index',        as: :authenticated_root
+        root to: 'home#index', as: :authenticated_root
     end
 
     unauthenticated do
@@ -17,10 +17,11 @@ Rails.application.routes.draw do
 
     resources :stamps, only: [:show]
 
+    get 'users/additional_info', to: 'users#additional_info', as: 'additional_info'
+    patch 'users/save_additional_info', to: 'users#save_additional_info', as: 'save_additional_info'
 
-    get     'users/additional_info',        to: 'users#additional_info',        as: 'additional_info'
-    patch   'users/save_additional_info',   to: 'users#save_additional_info',   as: 'save_additional_info'
-
-    get 'classes',                          to: 'classes#index',                as: 'classes'
-    get 'classes/:id',                      to: 'classes#show',                 as: 'class_diaries'
+    get 'classes', to: 'classes#index', as: 'classes'
+    get 'classes/:id', to: 'classes#show', as: 'class_diaries'
+    get 'classes/:id/class_diary', to: 'diaries#class_diary', as: 'class_diary'
+    get 'students/:id/student_diary', to: 'diaries#student_diary', as: 'student_diary'
 end
