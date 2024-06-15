1d9409736b42666787c2a71d63281de7a82ab41d
tama3br0 <newairport180@gmail.com>
2024-06-15 17:34:27 +0900

    ログインでのflash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,14 +5,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         if @user.additional_info_provided
           sign_in_and_redirect @user, event: :authentication
+          set_flash_message(:notice, :login_success, kind: 'Google') if is_navigational_format?
         else
           sign_in @user
           redirect_to additional_info_path
+          set_flash_message(:notice, :additional_info_required) if is_navigational_format?
         end
-        set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
         session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
-        redirect_to new_user_registration_url
+        redirect_to new_user_registration_url, alert: 'ログインできませんでした。 おうちの人にきくか、 あした がっこうで せんせいに きいてね！'
       end
     end
 end
