758b6cde3661691ded94df382f0dac51fba90fcc
tama3br0 <newairport180@gmail.com>
2024-06-15 21:36:39 +0900

    CSSを追加

--- app/helpers/diaries_helper.rb
+++ app/helpers/diaries_helper.rb
@@ -1,2 +1,9 @@
 module DiariesHelper
+    def display_user_info(user)
+        if user.role == 'teacher'
+          "#{user.name}せんせい"
+        else
+          "#{user.grade_class.grade}ねん#{user.grade_class.class_num}くみ #{user.student_num}ばん"
+        end
+    end
 end
