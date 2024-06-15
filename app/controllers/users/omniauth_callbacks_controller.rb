a6a6c6ca334fd62bcaa871ebe46b3dcc6458015f
tama3br0 <newairport180@gmail.com>
2024-06-15 17:56:27 +0900

    ログイン後のflash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -5,7 +5,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       if @user.persisted?
         if @user.additional_info_provided
           sign_in_and_redirect @user, event: :authentication
-          set_flash_message(:notice, :login_success, kind: 'Google') if is_navigational_format?
+          set_flash_message(:notice, :login_success) if is_navigational_format?
         else
           sign_in @user
           redirect_to additional_info_path
