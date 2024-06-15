546b99c6ea0f1b93510d48049ac709500db773c0
tama3br0 <newairport180@gmail.com>
2024-06-15 15:53:08 +0900

    answer[]を調整

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -14,27 +14,26 @@ class DiariesController < ApplicationController
       # すべての質問に対して回答が提供されているかをチェック
       Rails.logger.debug "params[:answers]の中身をチェック: #{params[:answers].inspect}"
       @questions = Question.all
-      missing_answers = @questions.any? { |q| params[:answers][q.id.to_s].blank? }
 
-      respond_to do |format|
+      if params[:answers].nil?
+        flash.now[:alert] = 'こたえていない しつもんがあるよ'
+        render :new
+      else
+        missing_answers = @questions.any? { |q| params[:answers][q.id.to_s].blank? }
+
         if missing_answers
           flash.now[:alert] = 'こたえていない しつもんがあるよ'
-          Rails.logger.debug "答えていない質問があります"
-          format.turbo_stream { render turbo_stream: turbo_stream.replace("flash_messages", partial: "layouts/flash") }
-          format.html { render :new }
+          render :new
         elsif @diary.save
           params[:answers].each do |question_id, choose_emotion_id|
             @diary.answers.create(question_id: question_id, choose_emotion_id: choose_emotion_id)
           end
 
           Stamp.create(user: current_user, diary: @diary)
-          format.turbo_stream { flash.now[:notice] = 'にっきを ていしゅつしました！' }
-          format.html { redirect_to stamp_path(current_user.id), notice: 'にっきを ていしゅつしました！' }
+          redirect_to stamp_path(current_user.id), notice: 'にっきを ていしゅつしました！'
         else
           flash.now[:alert] = @diary.errors.full_messages.join(', ')
-          Rails.logger.debug "日記の提出に失敗しました: #{@diary.errors.full_messages.join(', ')}"
-          format.turbo_stream { render turbo_stream: turbo_stream.replace("flash_messages", partial: "layouts/flash") }
-          format.html { render :new }
+          render :new
         end
       end
     end
