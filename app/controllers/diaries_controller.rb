f645da0391cf9d8f9110a6c3cbe0544599dd38ef
tama3br0 <newairport180@gmail.com>
2024-06-16 13:30:08 +0900

    role別にCRUD権限を付与

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,81 +1,54 @@
 class DiariesController < ApplicationController
     before_action :authenticate_user!
+    before_action :authorize_teacher, only: [:edit, :update, :new, :create]
 
     def new
-        @diary = Diary.new
-        @questions = Question.all
-        @selected_answers = {}
+      @diary = Diary.new(date: params[:date], user_id: params[:user_id])
+      @questions = Question.all
     end
 
     def create
-        current_user.diaries.where(date: diary_params[:date]).destroy_all
-        @diary = current_user.diaries.build(diary_params)
-        @questions = Question.all
-        @selected_answers = params[:answers] || {}
+      @diary = Diary.new(diary_params.merge(user_id: params[:diary][:user_id]))
+      @questions = Question.all
+      @selected_answers = params[:answers] || {}
 
-        if @selected_answers.empty? || @questions.any? { |q| @selected_answers[q.id.to_s].blank? }
-            flash.now[:alert] = 'こたえていない しつもんがあるよ'
-            render :new, status: :unprocessable_entity
+      if @selected_answers.empty? || @questions.any? { |q| @selected_answers[q.id.to_s].blank? }
+        flash.now[:alert] = 'こたえていない しつもんがあるよ'
+        render :new, status: :unprocessable_entity
+      else
+        if @diary.save
+          @selected_answers.each do |question_id, choose_emotion_id|
+            @diary.answers.create(question_id: question_id, choose_emotion_id: choose_emotion_id)
+          end
+          Stamp.create(user: @diary.user, diary: @diary)
+          redirect_to stamp_path(@diary.user.id), notice: 'にっきを ていしゅつしました！'
         else
-            if @diary.save
-                @selected_answers.each do |question_id, choose_emotion_id|
-                    @diary.answers.create(question_id: question_id, choose_emotion_id: choose_emotion_id)
-                end
-                Stamp.create(user: current_user, diary: @diary)
-                redirect_to stamp_path(current_user.id), notice: 'にっきを ていしゅつしました！'
-            else
-                flash.now[:alert] = @diary.errors.full_messages.join(', ')
-                render :new, status: :unprocessable_entity
-            end
+          flash.now[:alert] = @diary.errors.full_messages.join(', ')
+          render :new, status: :unprocessable_entity
         end
+      end
     end
 
-    def choose_diary
-        @date = params[:date]&.to_date || Date.today
-        start_of_week = @date.beginning_of_week(:monday)
-        end_of_week = @date.end_of_week(:sunday)
-
-        @previous_week_range = (start_of_week - 7.days)..(end_of_week - 7.days + 1.day)
-        @current_week_range = start_of_week..(end_of_week + 1.day)
-        @next_week_range = (start_of_week + 7.days)..(end_of_week + 7.days + 1.day)
-
-        @diaries = current_user.diaries.where(date: @current_week_range).order(:date)
+    def edit
+      @diary = Diary.find(params[:id])
     end
 
-    def class_diary
-        @grade_class = GradeClass.find(params[:id])
-        @date = params[:date]&.to_date || Date.today
-        @previous_date = @date - 1.day
-        @next_date = @date + 1.day
-
-        @students = User.where(grade_class: @grade_class).order(:student_num)
-        @questions = Question.all
-    end
-
-    def student_diary
-        @student = User.find(params[:id])
-        @date = params[:date]&.to_date || Date.today
-        @previous_month = @date - 1.month
-        @next_month = @date + 1.month
-
-        @grade = @student.grade_class.grade
-        @class_num = @student.grade_class.class_num
-        @student_num = @student.student_num
-
-        @diaries = @student.diaries.where("date >= ? AND date <= ?", @date.beginning_of_month, @date.end_of_month).order(:date)
-        @questions = Question.all
-        @user = @student  # 追加: @user に選択されたユーザー情報をセット
-    end
-
-
-    def show
-        @diary = current_user.diaries.find(params[:id])
-        @questions = @diary.answers.includes(:question, :choose_emotion).map(&:question).uniq
+    def update
+      @diary = Diary.find(params[:id])
+      if @diary.update(diary_params)
+        redirect_to @diary, notice: 'Diary was successfully updated.'
+      else
+        render :edit
+      end
     end
 
     private
 
+    def authorize_teacher
+      redirect_to(root_path, alert: 'You are not authorized to perform this action.') unless current_user.teacher?
+    end
+
     def diary_params
-        params.require(:diary).permit(:date, :image_url, answers: {})
+      params.require(:diary).permit(:date, :user_id, :image_url, answers: {})
     end
 end
