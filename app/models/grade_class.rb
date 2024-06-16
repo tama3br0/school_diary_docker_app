620b5db3c3e3fb423ac916fff897e35bc4945720
tama3br0 <newairport180@gmail.com>
2024-06-16 21:18:33 +0900

    モデルにバリデーションを追加

--- app/models/grade_class.rb
+++ app/models/grade_class.rb
@@ -1,2 +1,7 @@
 class GradeClass < ApplicationRecord
-end
+    has_many :users
+
+    validates :grade, presence: true
+    validates :class_num, presence: true
+    validates :school_code, presence: true
+  end
