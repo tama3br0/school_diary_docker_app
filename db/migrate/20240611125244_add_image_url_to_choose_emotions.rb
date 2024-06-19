5b7e9dfda4b06466c7499061c6853c2b23c4c4a3
tama3br0 <newairport180@gmail.com>
2024-06-19 15:54:53 +0900

    マイグレーションファイルの修正

--- db/migrate/20240611125244_add_image_url_to_choose_emotions.rb
+++ db/migrate/20240611125244_add_image_url_to_choose_emotions.rb
@@ -1,5 +1,7 @@
 class AddImageUrlToChooseEmotions < ActiveRecord::Migration[7.1]
-  def change
-    add_column :choose_emotions, :image_url, :string
-  end
+    def change
+      unless column_exists?(:choose_emotions, :image_url)
+        add_column :choose_emotions, :image_url, :string
+      end
+    end
 end
